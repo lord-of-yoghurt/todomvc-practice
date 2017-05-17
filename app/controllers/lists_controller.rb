7a90154b7fb64d1139fda7029559490dcac8da98
David Ordovskiy <david.ordovsky@gmail.com>
2017-05-16 22:50:20 -0400

    stub out the show view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,20 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
 
   def index
     @lists = List.all
     # render 'lists/index.html.erb'
   end
+
+  def show
+  end
+
+  private
+    def set_list
+      @list = List.find(params[:id])
+    end
+
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
